6fa63d3698daea95a264545fde7ffc6631311361
Aaron Borden <adborden@a14n.net>
2015-09-27 12:56:53 -0700

    Remove bower deps

--- karma.conf.js
+++ karma.conf.js
@@ -36,7 +36,7 @@ module.exports = function (config) {
 
         browserify: {
             debug: true,
-            transform: ['debowerify', 'partialify', istanbul({
+            transform: ['partialify', istanbul({
                 'ignore': ['**/*.spec.js', '**/thirdparty/**/*.js']
             })],
 
