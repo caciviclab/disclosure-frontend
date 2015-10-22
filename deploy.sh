f98d6e7f7cd4214efcd65efc17feeeb90103cc7d
Aaron Borden <adborden@a14n.net>
2015-10-21 23:36:30 -0700

    Deploy from Travis

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@ set -o pipefail
 GITHUB_REPO="github.com/caciviclab/disclosure-frontend-alpha"
 
 if [ -z "$GITHUB_TOKEN" ]; then
-    echo "GITHUB_TOKEN is required."
+    echo "GITHUB_TOKEN is required." >&2
     exit 1
 fi
 
@@ -18,9 +18,10 @@ gulp prod
 
 # Git init
 cd dist
+git --version
 git init
 git config user.name "CA Civic Lab deploy script"
 git config user.email "opencal@googlegroups.com"
 git add .
 git commit -m "Deploy to GitHub Pages"
-git push --force "https://${GITHUB_TOKEN}@${GITHUB_REPO}" master:gh-pages
+git push --force --quiet "https://x-api-token:${GITHUB_TOKEN}@${GITHUB_REPO}.git" master:gh-pages
