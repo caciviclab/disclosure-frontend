443a3e367eccfd007307a258d73e61c1ae2dc1c6
Aaron D Borden <adborden@a14n.net>
2015-12-14 21:23:34 -0800

    Break contributionByType into component

--- app/components/common/cityModule/index.js
+++ app/components/common/cityModule/index.js
@@ -1,6 +1,9 @@
 'use strict';
 
+require('../contributionTypeBreakdown');
+
 var cityModule = angular.module('cityModule', [
+    'contributionTypeBreakdown',
     'appMainModule'
   ])
   .controller('cityController', require('./cityController'));
