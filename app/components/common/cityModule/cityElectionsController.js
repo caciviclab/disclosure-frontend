4979e7d4854f5bb813158bbb615569bf9bfd94c7
Aaron D Borden <adborden@a14n.net>
2016-01-02 17:11:59 -0800

    Work around browserify issue
    Browserify doesn't like single quotes in the html, for some reason.

--- app/components/common/cityModule/cityElectionsController.js
+++ app/components/common/cityModule/cityElectionsController.js
@@ -2,6 +2,17 @@
 
 function cityElectionsController($scope, ballot) {
   $scope.ballot = ballot;
+
+  $scope.isOffice = isOffice;
+  $scope.isReferendum = isReferendum;
+
+  function isOffice(contest) {
+    return contest.type === 'office';
+  }
+
+  function isReferendum(contest) {
+    return contest.type === 'referendum';
+  }
 }
 
 module.exports = cityElectionsController;
