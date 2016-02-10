a43acd8d14211f1f8087445100f9800761e02759
Aaron D Borden <adborden@a14n.net>
2016-02-09 23:36:27 -0800

    Changes for newer angular 1.4.x versions

--- app/components/common/localityListing/localityListingDirective.spec.js
+++ app/components/common/localityListing/localityListingDirective.spec.js
@@ -35,12 +35,15 @@ describe('localityListingDirective', function() {
       }
     };
 
+    var ballot = {};
+
     angular.mock.inject(function($compile, $rootScope) {
       scope = $rootScope.$new();
+      scope.ballot = ballot;
       scope.disclosureSummary = disclosureSummary;
       scope.locality = locality;
 
-      localityListingEl = angular.element('<locality-listing locality="locality" disclosure-summary="disclosureSummary"></locality-listing>');
+      localityListingEl = angular.element('<locality-listing locality="locality" disclosure-summary="disclosureSummary" ballot="ballot"></locality-listing>');
       $compile(localityListingEl)(scope);
       $rootScope.$digest();
     });
