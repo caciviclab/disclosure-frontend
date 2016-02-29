f7111b9afbe553cdce2c3b79ef881b25dc91a084
jnmarcus <jnmarcus872@gmail.com>
2016-02-29 00:42:10 -0800

    change to lowercase e

--- app/components/appMainModule/examplePage1Module/ExamplePage1Controller.js
+++ app/components/appMainModule/examplePage1Module/ExamplePage1Controller.js
@@ -2,12 +2,13 @@
   'use strict';
   // Controller naming conventions should start with an uppercase letter
   function ExamplePage1Controller($scope, $log, TestFactory1, ExamplePage1Factory) {
+  function ExamplePage1Controller($scope, $log, TestFactory1, examplePage1Factory) {
     $scope.exampleContents1 = 'We are up and running using a required module!';
     $scope.paragraphText = ' Example Page 1 explicitly references required files';
     var committeeExample = {};
 
     function getCommitteeExampleForPage(committeeId, urlPaths) {
-      return ExamplePage1Factory.getExampleCommittee(committeeId, urlPaths)
+      return examplePage1Factory.getExampleCommittee(committeeId, urlPaths)
         .then(function(data) {
           committeeExample = data;
           $log.info('COMMITTEE EXAMPLE = ', committeeExample);
@@ -18,6 +19,6 @@
   }
 
   // $inject is necessary for minification. See http://bit.ly/1lNICde for explanation.
-  ExamplePage1Controller.$inject = ['$scope', '$log', 'TestFactory1', 'ExamplePage1Factory'];
+  ExamplePage1Controller.$inject = ['$scope', '$log', 'TestFactory1', 'examplePage1Factory'];
   module.exports = ExamplePage1Controller;
 })();
--- app/components/appMainModule/examplePage1Module/examplePage1.js
+++ app/components/appMainModule/examplePage1Module/examplePage1.js
@@ -1,13 +1,13 @@
 (function() {
   'use strict';
 
-  var ExamplePage1Factory = require('./ExamplePage1Factory');
+  var examplePage1Factory = require('./examplePage1Factory');
   var examplePage1Directive = require('./examplePage1Directive');
   var ExamplePage1Controller = require('./ExamplePage1Controller');
 
   module.exports = angular.module('examplePage1Module', [])
     .directive('examplePage1', examplePage1Directive)
     .controller('ExamplePage1Controller', ExamplePage1Controller)
-    .factory('ExamplePage1Factory', ExamplePage1Factory);
+    .factory('examplePage1Factory', examplePage1Factory);
 
 })();
