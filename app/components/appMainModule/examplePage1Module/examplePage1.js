7a52ca31a43d3aee33db992dd2c28bb97156c2c2
jnmarcus <jnmarcus872@gmail.com>
2016-03-22 00:31:11 -0700

    add file with test functions for now..

--- app/components/appMainModule/examplePage1Module/examplePage1.js
+++ app/components/appMainModule/examplePage1Module/examplePage1.js
@@ -1,13 +1,19 @@
 (function() {
   'use strict';
 
-  var examplePage1Factory = require('./examplePage1Factory');
+  /* Common Modules 'examplePage1Module' Depend on */
+  require('../../common/core/core');
+
+  /* Components of 'examplePage1Module' */
   var examplePage1Directive = require('./examplePage1Directive');
   var ExamplePage1Controller = require('./ExamplePage1Controller');
+  var TestFactory1 = require('./testService1');
+  var examplePage1Factory = require('./examplePage1Factory');
 
-  module.exports = angular.module('examplePage1Module', [])
+  module.exports = angular.module('examplePage1Module', ['coreModules'])
     .directive('examplePage1', examplePage1Directive)
     .controller('ExamplePage1Controller', ExamplePage1Controller)
+    .factory('TestFactory1', TestFactory1)
     .factory('examplePage1Factory', examplePage1Factory);
 
 })();
