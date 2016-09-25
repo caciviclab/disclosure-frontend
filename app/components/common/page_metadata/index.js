719abf47318c096b6002ebf1f8877da7cd64f12d
Aaron D Borden <adborden@a14n.net>
2016-09-24 19:49:05 -0700

    Allow setting of page metadata

--- app/components/common/page_metadata/index.js
+++ app/components/common/page_metadata/index.js
@@ -1,23 +1,34 @@
 'use strict';
 
+var _defaultPageDescription;
+
 angular.module('odca.page_metadata', [])
-  .run(function($rootScope, pageMetadata) {
+  .run(['$rootElement', '$rootScope', 'pageMetadata', function($rootElement, $rootScope, pageMetadata) {
+    // Save the initial description
+    var descriptionElement = findDescriptionElement($rootElement);
+    _defaultPageDescription = descriptionElement.attr('content');
 
     // Listen to route changes to update title from route config
-    $rootScope.$on('$routeChangeSuccess', function (event, current) {
-      pageMetadata(current && current.title || '');
+    $rootScope.$on('$stateChangeSuccess', function(event, toState) {
+      var data = toState && toState.data || {};
+      pageMetadata({
+        title: data.pageTitle,
+        description: data.pageDescription
+      });
     });
+  }])
+  .factory('pageMetadata', ['$rootElement', '$rootScope', function($rootElement, $rootScope) {
+    return function(metadata) {
+      metadata = metadata || {};
+      $rootScope.pageTitle = metadata.title ? metadata.title + ' | ' : '';
 
-  })
-  .factory('pageMetadata', function ($rootScope) {
-    return function (title) {
-      pageMetadata($rootScope, title);
+      var descriptionElement = findDescriptionElement($rootElement);
+      descriptionElement.attr('content', metadata.description || _defaultPageDescription);
     };
-  });
+  }]);
 
-
-function pageMetadata ($rootScope, title) {
-  $rootScope.page_title = title ? title + ' | ' : '';
+function findDescriptionElement($rootElement) {
+  return angular.element($rootElement[0].querySelector('meta[name="description"]'));
 }
 
 
