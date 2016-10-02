8d92d7c257baae249b6db084ba62546b932361ea
KyleW <Kyle.Warneck@gmail.com>
2016-10-02 14:46:11 -0700

    Add initial styling to committee page.

--- app/components/common/committeeListing/index.js
+++ app/components/common/committeeListing/index.js
@@ -4,53 +4,88 @@
    * Describes a single political committee.
    **/
 
-(function() {
-  'use strict';
+   (function() {
+    'use strict';
 
-   angular
-   .module('committeeListing', ['agGrid'])
-   .directive('committeeListing', committeeListingDirective);
+    angular
+    .module('committeeListing',[])
+    .directive('committeeListing', committeeListingDirective);
 
-   function committeeListingDirective() {
-    return {
-      restrict: 'E',
-      template: require('./committeeListing.html'),
-      controller: committeeListingController,
-      controllerAs: '$ctrl',
-      bindToController: true,
-      scope: {
-        committee: '=',
-        contributions: '=',
-      }
-    };
-  }
-
-  committeeListingController.$inject = ['$filter'];
-  function committeeListingController($filter) {
-    var ctrl = this;
-    ctrl.committeeTotal = 0;
-    var contributionsObjs;
-
-    // Convert resources to plain old objects to make ag-grid happy
-    contributionsObjs = ctrl.contributions.map(function(resource) {
-      ctrl.committeeTotal += resource.Tran_Amt1;
+    function committeeListingDirective() {
       return {
-        Tran_NamL: resource.Tran_NamL,
-        Tran_Amt1: $filter('currency')(resource.Tran_Amt1, "$", 0),
-        Tran_Date: resource.Tran_Date
+        restrict: 'E',
+        template: require('./committeeListing.html'),
+        controller: committeeListingController,
+        controllerAs: '$ctrl',
+        bindToController: true,
+        scope: {
+          committee: '=',
+          contributions: '=',
+        }
       };
-    });
+    }
 
-    ctrl.gridOptions = {
-      columnDefs: [
-      {headerName: 'Contributor', field: 'Tran_NamL'},
-      {headerName: 'Amount', field: 'Tran_Amt1'},
-      {headerName: 'Date', field: 'Tran_Date'}
-      ],
-      enableSorting: true,
-      enableFilter: true,
-      rowData: contributionsObjs
-    };  
-  }
+    committeeListingController.$inject = ['$filter'];
+    function committeeListingController($filter) {
+      var ctrl = this;
+      ctrl.committeeTotal = 0;
+      ctrl.contributionsSearch = '';
+      var contributionsObjs;
+
+      // Convert resources to plain old objects to make ag-grid happy
+      contributionsObjs = ctrl.contributions.map(function(resource) {
+        ctrl.committeeTotal += resource.Tran_Amt1;
+        return {
+          Tran_NamL: resource.Tran_NamL,
+          Tran_Amt1: resource.Tran_Amt1,
+          Tran_Date: resource.Tran_Date
+        };
+      });
+
+      ctrl.gridOptions = {
+        columnDefs: [
+          {
+            headerName: 'Contributor',
+            field: 'Tran_NamL',
+            filter: 'text',
+            quickFilterText: '',
+            suppressMenuColumnPanel: true,
+            suppressMenuFilterPanel: true,
+            suppressMenuMainPanel: true,
+            width: 600,
+            minWidth: 400
+          },
+          {
+            headerName: 'Amount',
+            field: 'Tran_Amt1',
+            maxWidth: 300,
+            width: 200,
+            cellFormatter: function(params) {
+              return $filter('currency')(params.value, "$", 0);
+            },
+            sort: 'desc',
+            suppressFilter: true,
+            suppressMenuColumnPanel: true,
+            suppressMenuFilterPanel: true,
+            suppressMenuMainPanel: true
+          },
+          {
+            headerName: 'Date',
+            field: 'Tran_Date',
+            maxWidth: 300,
+            width: 200,
+            suppressMenuColumnPanel: true,
+            suppressMenuFilterPanel: true,
+            suppressMenuMainPanel: true,
+          }
+        ],
+        headerHeight: 40,
+        enableSorting: true,
+        enableFilter: true,
+        rowData: contributionsObjs,
+        rowHeight: 40,
+        suppressCellSelection: true
+      };  
+    }
 
 })();
