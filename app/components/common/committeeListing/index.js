d72675ac3711352a47c50f050f3c128f4223f3f4
KyleW <Kyle.Warneck@gmail.com>
2016-10-05 09:02:27 -0700

    On contributions page, combine first and last name. Standardize capitalization.

--- app/components/common/committeeListing/index.js
+++ app/components/common/committeeListing/index.js
@@ -36,33 +36,35 @@
 
       // ag-grid requires fixed widths in pixels. Calculating them here to take all available space
       var odcaGridElement = document.querySelectorAll('.odca-grid')[0];
-      var gridWidth = odcaGridElement.offsetWidth;
+      gridWidth = odcaGridElement.offsetWidth;
 
       // Convert resources to plain old objects to make ag-grid happy
       contributionsObjs = ctrl.contributions.map(function(resource) {
+        // Total listed contributions
         ctrl.committeeTotal += resource.Tran_Amt1;
+
         return {
-          Tran_NamL: resource.Tran_NamL,
+          Name: getFriendlyName(resource),
           Tran_Amt1: resource.Tran_Amt1,
           Tran_Date: resource.Tran_Date
         };
-      });      
+      });
 
       ctrl.gridOptions = {
         columnDefs: [
           {
             headerName: 'Contributor',
-            field: 'Tran_NamL',
+            field: 'Name',
             filter: 'text',
-            width: Math.round(gridWidth * .6 * 100)/100,
+            width: Math.round(gridWidth * 0.6 * 100) / 100,
             suppressMenu: true
           },
           {
             headerName: 'Amount',
             field: 'Tran_Amt1',
-            width: Math.round(gridWidth * .2 * 100)/100,
+            width: Math.round(gridWidth * 0.2 * 100) / 100,
             cellFormatter: function(params) {
-              return $filter('currency')(params.value, "$", 0);
+              return $filter('currency')(params.value, '$', 0);
             },
             sort: 'desc',
             suppressMenu: true
@@ -70,7 +72,7 @@
           {
             headerName: 'Date',
             field: 'Tran_Date',
-            width: Math.round(gridWidth * .2 * 100)/100,
+            width: Math.round(gridWidth * 0.2 * 100) / 100,
             suppressMenu: true
           }
         ],
@@ -81,6 +83,29 @@
         rowHeight: 40,
         suppressMovableColumns: true
       };
+
+      // Combine first and last name, capitalize
+      function getFriendlyName(resource) {
+        var name = '';
+        var names = [];
+        if (resource.Tran_NamF) {
+          names = names.concat(resource.Tran_NamF.split(' '));
+        }
+        
+        if (resource.Tran_NamL) {
+          names = names.concat(resource.Tran_NamL.split(' '));
+        }
+        
+        names.forEach(function(newName) {
+          name += _.capitalize(newName);
+          name += ' ';
+        });
+        // Remove the extra space at the end
+        return name.substring(0, name.length - 1);
+      }
+
     }
+
+
 
 })();
