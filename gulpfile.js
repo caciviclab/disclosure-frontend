2d211c8ea80d28a0e13a59470530e729311173ab
KyleW <Kyle.Warneck@gmail.com>
2017-05-02 19:54:04 -0700

    Remove unsued tasks. Beautify gitfile

--- gulpfile.js
+++ gulpfile.js
@@ -58,13 +58,13 @@ var express = require('express'),
 // Server settings
 server.use(express.static(filePath.build.dest));
 // Redirects everything back to our index.html
-server.all('/*', function(req, res) {
+server.all('/*', function (req, res) {
     res.sendfile('/', {
         root: filePath.build.dest
     });
 });
 // uncomment the "middleware" section when you are ready to connect to an API
-gulp.task('server', function() {
+gulp.task('server', function () {
     connect.server({
         root: filePath.build.dest,
         fallback: filePath.build.dest + '/index.html',
@@ -87,7 +87,7 @@ gulp.task('server', function() {
 // =======================================================================
 // Clean out dist folder contents on build
 // =======================================================================
-gulp.task('clean-dev', function() {
+gulp.task('clean-dev', function () {
     del(['./dist/*.js',
         './dist/css/*.css',
         '!./dist/fonts/**',
@@ -97,33 +97,35 @@ gulp.task('clean-dev', function() {
         './dist/*.png',
         './dist/*.ico',
         './reports/**/*',
-        './reports']);
+        './reports'
+    ]);
 });
 
-gulp.task('clean-full', function() {
+gulp.task('clean-full', function () {
     del(['./dist/*',
         './reports/**/*',
-        './reports']);
+        './reports'
+    ]);
 });
 
 
 // =======================================================================
 // JSHint
 // =======================================================================
-gulp.task('lint', function() {
+gulp.task('lint', function () {
     return gulp.src(filePath.lint.src)
-    .pipe(jshint())
-    .pipe(jshint.reporter(stylish));
+        .pipe(jshint())
+        .pipe(jshint.reporter(stylish));
 });
 
 
 // =======================================================================
 // Javascript Checkstyles (JSCS)
 // =======================================================================
-gulp.task('checkstyle', function() {
+gulp.task('checkstyle', function () {
     return gulp.src(filePath.lint.src)
-    .pipe(jscs())
-    .on('error', handleError);
+        .pipe(jscs())
+        .on('error', handleError);
 });
 
 
@@ -131,39 +133,37 @@ gulp.task('checkstyle', function() {
 // Browserify Bundle
 // =======================================================================
 
-function bundle(bundler) {
-    // Add options to add to "base" bundler passed as parameter
-    return bundler
-      .transform(browserifyCss, {global: true})
-      .transform(markedify)
-      .bundle()                                                        // Start bundle
-      .pipe(source(filePath.browserify.src))                        // Entry point
-      .pipe(buffer())                                               // Convert to gulp pipeline
-      .pipe(rename(filePath.browserify.outputFile))          // Rename output from 'app.js'
-                                                                              //   to 'bundle.js'
-      .pipe(sourceMaps.init({ loadMaps : true }))  // Strip inline source maps
-      .pipe(sourceMaps.write(filePath.browserify.mapDir))    // Save source maps to their
-                                                                       //   own directory
-      .pipe(gulp.dest(filePath.browserify.outputDir))        // Save 'bundle' to build/
-      .pipe(connect.reload());                              // Reload browser if relevant
-
-      //TODO: add this code in for bundling dev vs prod
-      //
+function bundle() {
+    return browserify({
+        entries: filePath.browserify.src, // Pass browserify the entry point
+        debug: true
+    })
+    .transform(browserifyCss, {
+            global: true
+        })
+        .transform(markedify)
+        .bundle() // Start bundle
+        .pipe(source(filePath.browserify.src)) // Entry point
+        .pipe(buffer()) // Convert to gulp pipeline
+        .pipe(rename(filePath.browserify.outputFile)) // Rename output from 'app.js' to 'bundle.js'
+        .pipe(sourceMaps.init({
+            loadMaps: true
+        })) // Strip inline source maps
+        .pipe(sourceMaps.write(filePath.browserify.mapDir)) // Save source maps to their own directory
+        .pipe(gulp.dest(filePath.browserify.outputDir)) // Save 'bundle' to build/
+        .pipe(connect.reload()); // Reload browser if relevant
 }
 
-gulp.task('bundle', function() {
-    var bundler = browserify({
-      entries: filePath.browserify.src, // Pass browserify the entry point
-      debug: true
-    });
-    bundle(bundler).pipe(gulpif(!bundle.prod, sourceMaps.init({
-                  loadMaps: true
-              })))
-              .pipe(gulpif(!bundle.prod, sourceMaps.write('./')))
-              .pipe(gulpif(bundle.prod, streamify(uglify({
-                  mangle: false
-              }))))
-// ;  // Chain other options -- sourceMaps, rename, etc.
+gulp.task('bundle', function () {
+    bundle().pipe(
+            gulpif(!bundle.prod, sourceMaps.init({
+                loadMaps: true
+            }))
+        )
+        .pipe(gulpif(!bundle.prod, sourceMaps.write('./')))
+        .pipe(gulpif(bundle.prod, streamify(uglify({
+            mangle: false
+        }))))
 });
 
 
@@ -171,47 +171,47 @@ gulp.task('bundle', function() {
 // =======================================================================
 // Styles Task
 // =======================================================================
-gulp.task('styles-dev', function() {
+gulp.task('styles-dev', function () {
     return gulp.src(filePath.styles.src)
-    .pipe(sourceMaps.init())
-    .pipe(less())
-    .on('error', handleError)
-    .pipe(sourceMaps.write())
-    .pipe(gulp.dest(filePath.build.dest + '/css'))
-    .on('error', handleError)
-    .pipe(notify({
-        message: 'Styles task complete'
-    }))
-    .pipe(connect.reload());
+        .pipe(sourceMaps.init())
+        .pipe(less())
+        .on('error', handleError)
+        .pipe(sourceMaps.write())
+        .pipe(gulp.dest(filePath.build.dest + '/css'))
+        .on('error', handleError)
+        .pipe(notify({
+            message: 'Styles task complete'
+        }))
+        .pipe(connect.reload());
 });
 
-gulp.task('styles-prod', function() {
+gulp.task('styles-prod', function () {
     return gulp.src(filePath.styles.src)
-    .pipe(less())
-    .on('error', handleError)
-    .pipe(prefix('last 1 version', '> 1%', {
-        map: true
-    }))
-    .pipe(cssnano())
-    .pipe(gulp.dest(filePath.build.dest + '/css'))
-    .on('error', handleError)
-    .pipe(notify({
-        message: 'Styles task complete'
-    }));
+        .pipe(less())
+        .on('error', handleError)
+        .pipe(prefix('last 1 version', '> 1%', {
+            map: true
+        }))
+        .pipe(cssnano())
+        .pipe(gulp.dest(filePath.build.dest + '/css'))
+        .on('error', handleError)
+        .pipe(notify({
+            message: 'Styles task complete'
+        }));
 });
 
 
 // =======================================================================
 // Images Task
 // =======================================================================
-gulp.task('images', function() {
+gulp.task('images', function () {
     return gulp.src(filePath.assets.images.src)
-    .on('error', handleError)
-    .pipe(gulp.dest(filePath.assets.images.dest))
-    .pipe(notify({
-        message: 'Images copied'
-    }))
-    .pipe(connect.reload());
+        .on('error', handleError)
+        .pipe(gulp.dest(filePath.assets.images.dest))
+        .pipe(notify({
+            message: 'Images copied'
+        }))
+        .pipe(connect.reload());
 });
 
 
@@ -244,32 +244,32 @@ gulp.task('fonts', function () {
 // =======================================================================
 // Copy index.html
 // =======================================================================
-gulp.task('copyIndex', function() {
+gulp.task('copyIndex', function () {
     return gulp.src(filePath.copyIndex.src)
-    .pipe(gulp.dest(filePath.build.dest))
-    .pipe(notify({
-        message: 'index.html successfully copied'
-    }))
-    .pipe(connect.reload());
+        .pipe(gulp.dest(filePath.build.dest))
+        .pipe(notify({
+            message: 'index.html successfully copied'
+        }))
+        .pipe(connect.reload());
 });
 
 
 // =======================================================================
 // Copy Favicon
 // =======================================================================
-gulp.task('copyFavicon', function() {
+gulp.task('copyFavicon', function () {
     return gulp.src(filePath.copyFavicon.src)
-    .pipe(gulp.dest(filePath.build.dest))
-    .pipe(notify({
-        message: 'favicon successfully copied'
-    }));
+        .pipe(gulp.dest(filePath.build.dest))
+        .pipe(notify({
+            message: 'favicon successfully copied'
+        }));
 });
 
 
 // =======================================================================
 // Watch for changes
 // =======================================================================
-gulp.task('watch', function() {
+gulp.task('watch', function () {
     gulp.watch(filePath.styles.watch, ['styles-dev']);
     gulp.watch(filePath.assets.images.watch, ['images']);
     gulp.watch(filePath.browserify.watch, ['bundle']);
@@ -282,24 +282,24 @@ gulp.task('watch', function() {
 // =======================================================================
 // Start a webserver to serve the assets to developers directly
 // =======================================================================
-gulp.task('webserver', function() {
-  gulp.src('app')
-  .pipe(webserver({
-    livereload: true,
-    directoryListing: true,
-    open: true
-  }));
+gulp.task('webserver', function () {
+    gulp.src('app')
+        .pipe(webserver({
+            livereload: true,
+            directoryListing: true,
+            open: true
+        }));
 });
 
 // =======================================================================
 // Karma Configuration
 // =======================================================================
-gulp.task('karma', function(done) {
-  var karma = new Karma({
-    configFile: __dirname + '/karma.conf.js',
-    singleRun: !argv.watch
-  }, done);
-  karma.start();
+gulp.task('karma', function (done) {
+    var karma = new Karma({
+        configFile: __dirname + '/karma.conf.js',
+        singleRun: !argv.watch
+    }, done);
+    karma.start();
 });
 
 
@@ -308,50 +308,42 @@ gulp.task('karma', function(done) {
 // =======================================================================
 
 // run "gulp" in terminal to build the DEV app
-gulp.task('build-dev', function(callback) {
+gulp.task('build-dev', function (callback) {
     runSequence(
         ['clean-dev', 'lint', 'checkstyle'],
         // images and vendor tasks are removed to speed up build time. Use "gulp build" to do a full re-build of the dev app.
         [
-          // 'bundle-dev',
-          'bundle',
-          'styles-dev', 'copyIndex', 'copyFavicon'], ['server', 'watch'],
+            'bundle',
+            'styles-dev', 'copyIndex', 'copyFavicon'
+        ], ['server', 'watch'],
         callback
     );
 });
 
 // run "gulp test" in terminal to build the DEV app
-gulp.task('build-test', function(callback) {
+gulp.task('build-test', function (callback) {
     runSequence(
-        ['clean-full', 'lint', 'checkstyle'],
-        ['karma'],
+        ['clean-full', 'lint', 'checkstyle'], ['karma'],
         callback
     );
 });
 
 // run "gulp prod" in terminal to build the PROD-ready app
-gulp.task('build-prod', function(callback) {
+gulp.task('build-prod', function (callback) {
     runSequence(
-        ['clean-full', 'lint', 'checkstyle'],
-        [
-          // 'bundle-prod',
-          'bundle',
-          'styles-prod', 'images', 'fonts','copyIndex', 'copyFavicon'],
+        ['clean-full', 'lint', 'checkstyle'], [
+            'bundle',
+            'styles-prod', 'images', 'fonts', 'copyIndex', 'copyFavicon'
+        ],
         callback
     );
 });
 
 // run "gulp build" in terminal for a full re-build in DEV
-gulp.task('build', function(callback) {
+gulp.task('build', function (callback) {
     runSequence(
-      ['clean-full'
-        // 'lint',
-        // 'checkstyle'
-      ],
-        [
-          // 'bundle-dev',
-          'bundle',
-          'styles-dev', 'images', 'fonts','copyIndex', 'copyFavicon'],
+        ['clean-full', 'lint','checkstyle'],
+        ['bundle','styles-dev', 'images', 'fonts', 'copyIndex', 'copyFavicon'],
         ['server', 'watch'],
         callback
     );
